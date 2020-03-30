51f549be092be1b1b18c253c87f9527c4a9959ab
Poafs1 <poafs1@hotmail.com>
2020-03-31 01:07:24 +0700

    Initial Commit

--- fillfeel/app/src/main/java/com/example/fillfeel/CustomAdapter.kt
+++ fillfeel/app/src/main/java/com/example/fillfeel/CustomAdapter.kt
@@ -1,16 +1,30 @@
 package com.example.fillfeel
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.card_view.view.*
 
+
 class CustomAdapter (
     private val data: List<ExploreObject>
 ) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    @Throws(IllegalArgumentException::class)
+    fun convert(base64Str: String): Bitmap? {
+        val decodedBytes: ByteArray = Base64.decode(
+            base64Str.substring(base64Str.indexOf(",") + 1),
+            Base64.DEFAULT
+        )
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+    }
 
     private val items: MutableList<CardView>
 
@@ -27,6 +41,7 @@ class CustomAdapter (
 
     override fun onBindViewHolder(holder: CustomAdapter.ViewHolder, position: Int) {
         holder.eventTitle.text = data[position].title
+        holder.eventImage.setImageBitmap(convert(data[position].img))
 
         items.add(holder.card)
     }
@@ -38,6 +53,7 @@ class CustomAdapter (
         itemView: View
     ) : RecyclerView.ViewHolder(itemView) {
         val eventTitle: TextView = itemView.event_title
+        val eventImage: ImageView = itemView.event_img
         val card: CardView = itemView.card
     }
 }
