8ef9ffdd753079961facbd47ca1591ab97d12014
Poafs1 <poafs1@hotmail.com>
2020-04-02 21:10:16 +0700

    Init Authentication

--- fillfeel/app/src/main/java/com/example/fillfeel/HistoryFragment.kt
+++ fillfeel/app/src/main/java/com/example/fillfeel/HistoryFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 /**
  * A simple [Fragment] subclass.
  */
 class HistoryFragment : Fragment() {
+
+    private lateinit var bottomNavigation: BottomNavigationView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +20,12 @@ class HistoryFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_history, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bottomNavigation = activity!!.findViewById(R.id.bottom_navigation)
+        bottomNavigation.menu.getItem(1).isChecked = true
     }
 
 }
