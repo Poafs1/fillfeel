e8b622cf76953acb042a4e23d7c83d8f23c62a7b
Poafs1 <poafs1@hotmail.com>
2020-05-07 02:03:32 +0700

    .

--- fillfeel/app/src/main/java/com/example/fillfeel/MainActivity.kt
+++ fillfeel/app/src/main/java/com/example/fillfeel/MainActivity.kt
@@ -36,8 +36,10 @@ import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager
 import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateRemoteModel
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions
 import com.jakewharton.threetenabp.AndroidThreeTen
@@ -172,6 +174,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        val modelManager = FirebaseModelManager.getInstance()
+        val deModel1 = FirebaseTranslateRemoteModel.Builder(FirebaseTranslateLanguage.EN).build()
+        modelManager.deleteDownloadedModel(deModel1)
+            .addOnSuccessListener {
+                // Model deleted.
+            }
+            .addOnFailureListener { exception ->
+                Log.e(TAG, exception.toString())
+            }
+
+        val deModel2 = FirebaseTranslateRemoteModel.Builder(FirebaseTranslateLanguage.TH).build()
+        modelManager.deleteDownloadedModel(deModel2)
+            .addOnSuccessListener {
+                // Model deleted.
+            }
+            .addOnFailureListener { exception ->
+                Log.e(TAG, exception.toString())
+            }
+
 //        settingTitle = findViewById(R.id.settingTitle)
 //        accountSettingTitle = findViewById(R.id.accountSettingTitle)
 //        changeLanguageSettingTitle = findViewById(R.id.changeLanguageSettingTitle)
@@ -180,27 +202,27 @@ class MainActivity : AppCompatActivity() {
 
         initTranslation()
         AndroidThreeTen.init(this)
-
-        // Firebase Realtime Database for Offline Mode and keep it refresh in some table
-        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-        val eventsRef = Firebase.database.getReference("events")
-        val usersRef = Firebase.database.getReference("users")
-        eventsRef.keepSynced(true)
-        usersRef.keepSynced(true)
-
+//
+//        // Firebase Realtime Database for Offline Mode and keep it refresh in some table
+//        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+//        val eventsRef = Firebase.database.getReference("events")
+//        val usersRef = Firebase.database.getReference("users")
+//        eventsRef.keepSynced(true)
+//        usersRef.keepSynced(true)
+//
         setContentView(R.layout.activity_main)
         val fragmentManager: FragmentManager = supportFragmentManager
         val currentFragment: Fragment? =
             fragmentManager.findFragmentById(R.id.root_layout)
-
+//
         authenticationPage = findViewById(R.id.authentication_page)
         mCallbackManager = CallbackManager.Factory.create()
-
-        // Firebase Authentication Init
+//
+//        // Firebase Authentication Init
         auth = FirebaseAuth.getInstance()
         mDatabase = FirebaseDatabase.getInstance().getReference()
         user = auth.currentUser!!
-
+//
         mDatabase
             .child("users")
             .child(user.uid).child("lang")
@@ -215,13 +237,13 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             })
-
+//
         gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-
+//
         LoginManager.getInstance().registerCallback(mCallbackManager, object :
             FacebookCallback<LoginResult> {
             override fun onSuccess(loginResult: LoginResult) {
@@ -237,27 +259,27 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG, "facebook:onError", error)
             }
         })
-
-        // Hidden Toolbar, Bottom Navigation Bar, and Logo in Sign In Page
+//
+//        // Hidden Toolbar, Bottom Navigation Bar, and Logo in Sign In Page
         handleToolbar()
         handleBottomNavigationBar()
         handleLogo()
-
-        // On Click Listener for Google Sign In Method Button
+//
+//        // On Click Listener for Google Sign In Method Button
         val googleButton: Button = findViewById(R.id.google_button)
         googleButton.setOnClickListener{view ->
             method = "google"
             signIn()
         }
-
-        // On Click Listener for Facebook Sign In Method Button
+//
+//        // On Click Listener for Facebook Sign In Method Button
         val facebookButton: Button = findViewById(R.id.facebook_button)
         facebookButton.setOnClickListener{view ->
             method = "facebook"
             LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email", "public_profile"))
             overridePendingTransition(0, 0)
         }
-
+//
         val nightMode = getResources().configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
         handleNighMode(nightMode)
         handleBottomSheetDialog(currentFragment)
